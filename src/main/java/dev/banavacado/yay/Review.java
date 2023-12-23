897a6ddff979bb0befd470124c80e60407cbe918
Duy <diidduong@gmail.com>
2023-12-23 12:54:18 -0700

    fix collation to collection, add constructor without id

--- src/main/java/dev/banavacado/yay/Review.java
+++ src/main/java/dev/banavacado/yay/Review.java
@@ -3,15 +3,20 @@ package dev.banavacado.yay;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collation = "reviews")
+@Document(collection = "reviews")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Review {
     @Id
-    private Object id;
+    private ObjectId id;
     private String body;
+
+    public Review(String body) {
+        this.body = body;
+    }
 }
