174d5439b6f67b95cd2012627db3a0d8a6d9dd85
Duy <diidduong@gmail.com>
2023-12-23 12:55:16 -0700

    setup controller, service, repository to write review

---
+++ src/main/java/dev/banavacado/yay/ReviewRepository.java
@@ -0,0 +1,10 @@
+package dev.banavacado.yay;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+
+}
